9edcf5de10bf2a5ee78a4540a968ab3fb8ffd7bb
aluno <aluno@ifpe>
2022-09-29 09:27:14 -0300

    enviando os primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood_marialima/OxefoodMarialimaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_marialima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodMarialimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodMarialimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefood_marialima/OxefoodMarialimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_marialima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodMarialimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
