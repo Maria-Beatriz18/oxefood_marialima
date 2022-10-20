33843115752a2d11a396a092f13da8f1d177af37
aluno <aluno@ifpe>
2022-10-20 11:52:48 -0300

    Adicionando classes para a funcionalidade de Cliente:

---
+++ src/main/java/br/com/ifpe/oxefood_marialima/modelo/empresa/Empresa.java
@@ -0,0 +1,5 @@
+package br.com.ifpe.oxefood_marialima.modelo.empresa;
+
+public class Empresa {
+
+}
---
+++ src/main/java/br/com/ifpe/oxefood_marialima/servicos/empresa/EmpresaController.java
@@ -0,0 +1,5 @@
+package br.com.ifpe.oxefood_marialima.servicos.empresa;
+
+public class EmpresaController {
+
+}
